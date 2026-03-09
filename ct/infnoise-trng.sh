f72976b8f8873b566f304f5b3a38d61e3e9f2e6b
Aaron Fillmore <me@w33t.io>
2026-03-09 11:29:55 -0400

    Fix Proxmox helper shellcheck warnings

--- ct/infnoise-trng.sh
+++ ct/infnoise-trng.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC1090,SC1091,SC2154
+# shellcheck disable=SC1090,SC1091,SC2034,SC2154
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 set -euo pipefail
 
